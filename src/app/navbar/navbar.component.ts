dbafbb4534ac4d89d8fa1d5a4be3d8bc729e045e
Deepak Fletcher <deepakraofletcher@live.com>
2024-10-17 09:22:53 +0530

    styling changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  public navButton = navButtons;
+}
 
+
+export enum navButtons{
+about='About',
+education='Education',
+experience='Experience',
+contact='Contact',
 }
