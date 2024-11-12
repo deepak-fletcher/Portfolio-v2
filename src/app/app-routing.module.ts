d2e8072301d3e75a8f270202679eaec835d70e01
Deepak Fletcher <deepakraofletcher@live.com>
2024-11-12 15:15:24 +0530

    adding navigation to all the main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: LandingPageComponent },
   { path: 'contact', component: ContactMeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'education', component: EducationComponent },
 ];
 
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,32 +6,44 @@ import { RouterServiceService } from '../services/router-service.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   public navButton = navButtons;
   public faHomeIcon = faHome;
 
-  constructor(private router: Router, private routerService: RouterServiceService){
-  }
+  constructor(
+    private router: Router,
+    private routerService: RouterServiceService
+  ) {}
 
   public ngOnInit(): void {
     this.routerService.newRouteDetected.subscribe((val) => {
-        console.log(val)
-    })
+      console.log(val);
+    });
   }
 
-  public reRoute(toPage: navButtons){
-    if(toPage === navButtons.contact){
-      this.router.navigate(['/contact'])
+  public navigate(toPage: navButtons) {
+    switch (toPage) {
+      case navButtons.contact:
+        this.router.navigate(['/contact']);
+        break;
+      case navButtons.about:
+        this.router.navigate(['/about']);
+        break;
+      case navButtons.experience:
+        this.router.navigate(['/experience']);
+        break;
+      case navButtons.education:
+        this.router.navigate(['/education']);
+        break;
     }
   }
 }
 
-
-export enum navButtons{
-about='About',
-education='Education',
-experience='Experience',
-contact='Contact',
+export enum navButtons {
+  about = 'About',
+  education = 'Education',
+  experience = 'Experience',
+  contact = 'Contact',
 }
