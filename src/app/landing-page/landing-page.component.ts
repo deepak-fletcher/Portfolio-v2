88ba1bfa9c17443edfca530aaa467974e024c97c
Deepak Fletcher <deepakraofletcher@live.com>
2024-11-12 17:01:07 +0530

    adding bitmoji animation

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
-
+export class LandingPageComponent implements AfterViewInit {
+  public showBitmoji = false;
+  ngAfterViewInit(): void {
+    setTimeout(()=>{
+      this.showBitmoji = true;
+    })
+  }
 }
