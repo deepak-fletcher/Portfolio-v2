6d3b865e56e8de7f89f5e733a60aa0e8de5c9fee
Deepak Fletcher <deepakraofletcher@live.com>
2024-11-13 11:32:27 +0530

    adding changing bitmojis

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,9 +7,46 @@ import { AfterViewInit, Component } from '@angular/core';
 })
 export class LandingPageComponent implements AfterViewInit {
   public showBitmoji = false;
+  public showName = false;
+  public showMoreInfo = false;
+  public hoveringBitmoji = false;
+  public disablePointerevents = false;
+  public hoveringTimer: any;
+  public bitmojiPath = "../../assets/bitmoji.png"
+
   ngAfterViewInit(): void {
     setTimeout(()=>{
       this.showBitmoji = true;
+      this.showName = true;
     })
+  }
+
+  public hovering(){
+    this.hoveringTimer = setTimeout(()=>{
+      this.hoveringBitmoji = true;
+    }, 700)
+  }
+
+  public hoveringComplete(){
+    clearTimeout(this.hoveringTimer);
+    this.hoveringBitmoji = false;
+    this.showBitmoji = false;
+    setTimeout(()=>{
+      this.showBitmoji = true;
+    })
+  }
+
+  public showMore(){
+    this.disablePointerevents = true;
+    if(this.showMoreInfo){
+      this.showMoreInfo = false;
+      this.bitmojiPath = "../../assets/bitmoji.png"
+    }else{
+      this.showMoreInfo = true;
+      this.bitmojiPath = "../../assets/professionalBitmoji.png"
+    }
+    setTimeout(()=>{
+      this.disablePointerevents = false;
+    }, 1000)
   }
 }
