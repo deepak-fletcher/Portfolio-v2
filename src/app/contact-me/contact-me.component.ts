b2b8de4879d327b96cc5c7193af53ce88743c20c
Deepak Fletcher <deepakraofletcher@live.com>
2024-12-28 12:53:58 +0530

    adding spinner loader for image loading

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent {
+export class ContactMeComponent implements OnInit {
   public logos = [
     'assets/facebooklogo.png',
     'assets/instagramlogo.png',
     'assets/linkedinlogo.png',
     'assets/twitterlogo.png'
   ];
+
+  public loadingComplete = false;
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    this.preloadImages();
+  }
+
+  private preloadImages(): void {
+    this.logos.forEach((logo, index) => {
+      const img = new Image();
+      img.src = logo;
+      img.onload = () => this.onImageLoad(index);
+    });
+  }
+
+  public onImageLoad(position: number){
+    if(position === this.logos.length - 1){
+      this.loadingComplete = true;
+    }
+  }
 }
